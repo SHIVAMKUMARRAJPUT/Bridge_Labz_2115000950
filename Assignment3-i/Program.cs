715079d3363564c7844575fc0106f012e603e65a
unknown <shivamrajput81202@gmail.com>
2025-01-23 23:03:07 +0530

    Assignment 3(i)

---
+++ Assignment3-i/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+class Program
+{
+ static void Main(){
+    CheckNumber.print();
+    Console.ReadKey();
+    CountDownUsingFor.print();
+    Console.ReadKey();
+    CountDownUsingWhile.print();
+    Console.ReadKey();
+    DivisibleBy5.isDivisible();
+    Console.ReadKey();
+    EligibleForVote.canVote();
+    Console.ReadKey();
+    FactorialUsingFor.print();
+    Console.ReadKey();
+    FactorialUsingWhile.print();
+    Console.ReadKey();
+    FindBonus.print();
+    Console.ReadKey();
+    FirstSmallest.isSmallest();
+    Console.ReadKey();
+    LargestOfThree.isLargest();
+    MultiplicationTable.print();
+    Console.ReadKey();
+    NaturalNumber.print();
+    Console.ReadKey();
+    OddEven.print();
+    Console.ReadKey();
+    SpringSeason.print();
+    Console.ReadKey();
+    SumNaturalUsingForLoop.print();
+    Console.ReadKey();
+    SumNaturalUsingLoop.print();
+    Console.ReadKey();
+    SumTillExit.print();
+    Console.ReadKey();
+    SumTillExit1.print();
+    Console.ReadKey();
+
+
+    }   
+}
