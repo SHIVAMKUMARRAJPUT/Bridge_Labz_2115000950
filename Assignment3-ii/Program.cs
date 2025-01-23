2b41631864f96413e666b2b936649d579e8a35c4
unknown <shivamrajput81202@gmail.com>
2025-01-23 18:50:55 +0530

    Assginment 3(ii)

---
+++ Assignment3-ii/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.ReadKey();
+        prob_01.fun();
+        Console.ReadKey();
+        prob_02.leap();
+        Console.ReadKey();
+        Prob_03.GradeChecker();
+        Console.ReadKey();
+    }
+    
+}
---
+++ Assignment3-ii/prob_04.cs
@@ -0,0 +1,23 @@
+class prob_04{
+    public static void isPrime(){
+        Console.Write("Enter a number greater than 1: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num <= 1){
+            Console.WriteLine("Prime numbers are greater than 1.");
+            return;
+        }
+        bool isPrime = true;
+        for (int i = 2; i < num; i++){
+            if (num % i == 0){
+                isPrime = false;
+                break; 
+            }
+        }
+        if (isPrime){
+            Console.WriteLine($"{num} is a Prime Number.");
+        }else{
+            Console.WriteLine($"{num} is NOT a Prime Number.");
+        }
+    }
+
+}
