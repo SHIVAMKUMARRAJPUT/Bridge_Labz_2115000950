e9b43d8fe0545da711c6c8991912f75994764a30
unknown <shivamrajput81202@gmail.com>
2025-01-23 22:20:46 +0530

    Assignment 3(iii)

--- Assignment3-iii/Program.cs
+++ Assignment3-iii/Program.cs
@@ -2,30 +2,12 @@
 class Program
 {
     static void Main(){
-    prob_01.Armstrong();
-    Console.ReadKey();
-    prob_02.digitCount();
-    Console.ReadKey();
-    prob_03.harshadNumber();
-    Console.ReadKey();
-    prob_04.abundantNumber();
-    Console.ReadKey();
-    prob_05.dayCalculator();
-    Console.ReadKey();
-    prob_06.Calculator();
-    Console.ReadKey();
-    prob_07.BMICalcu();
-    Console.ReadKey();
-    prob_08.tallestHeight();
-    Console.ReadKey();
-    prob_09.greatestFactor();
-    Console.ReadKey();
-    prob_10.powercalcu();
-    Console.ReadKey();
-    prob_11.factorFind();
-    Console.ReadKey();
-    prob_12.multiples();
-
+        prob_01.Armstrong();
+        prob_02.digitCount();
+        prob_03.harshadNumber();
+        prob_04.abundantNumber();
+        prob_05.dayCalculator();
+        prob_06.Calculator();
     }
     
 }
