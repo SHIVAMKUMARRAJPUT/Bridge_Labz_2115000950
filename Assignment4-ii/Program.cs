5c3b8340ac80175af2f8e13ef634a54ba88400be
unknown <shivamrajput81202@gmail.com>
2025-01-27 13:51:38 +0530

    Assignment 4-ii

---
+++ Assignment4-ii/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main(){
+        FriendsComparison.Print();
+        BMICalculator.Print();
+        BMICalculator2DArray.Print();
+        DigitFrequency.print();
+        LargestDigit.Print();
+        LargestSecondLargest.Print();
+        ReverseNumber.Print();
+        SalaryBonus.Print();
+        StudentGrades.print();
+        StudentGrades2DArray.print();
+    }
+}
