485fafbb825f237d88603ed3fc368091f5ac45c6
unknown <shivamrajput81202@gmail.com>
2025-01-28 22:58:02 +0530

    Assignment5(iii)

---
+++ Assignment5-iii/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(){
+        // FootballTeam.print();
+        //    NumberChecker.print();
+        //    NumberChecker1.print();
+        //    NumberChecker2.print();
+        //    NumberChecker3.print();
+        //    NumberChecker4.print();
+        //    OTPGenerator.print();
+        //    CalendarProgram.print();
+        // EuclideanDistance.print();
+        // Collinear.print();
+        // Employee.print();
+        // StudentReport.print();
+        MatrixOperations.print();
+    }
+}
