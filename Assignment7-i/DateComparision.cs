9a5d73e5110090ec40bb1377470fe0e09785d51a
unknown <shivamrajput81202@gmail.com>
2025-01-29 23:27:23 +0530

    Assignment7-i(builtIn)

---
+++ Assignment7-i/DateComparision.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class DateComparision
+{
+    public static void print()
+    {
+        Console.Write("Enter the first date (yyyy-MM-dd): ");
+        DateTime firstDate = Convert.ToDateTime(Console.ReadLine());
+
+        Console.Write("Enter the second date (yyyy-MM-dd): ");
+        DateTime secondDate = Convert.ToDateTime(Console.ReadLine());
+
+        int comparisonResult = DateTime.Compare(firstDate, secondDate);
+
+        if (comparisonResult < 0)
+        {
+            Console.WriteLine("The first date is before the second date.");
+        }
+        else if (comparisonResult > 0)
+        {
+            Console.WriteLine("The first date is after the second date.");
+        }
+        else
+        {
+            Console.WriteLine("The two dates are the same.");
+        }
+    }
+}
+
