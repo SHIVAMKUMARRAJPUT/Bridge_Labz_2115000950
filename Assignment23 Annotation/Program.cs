0e13ef15542370d43d4f4db8e0a44da0592d1c49
unknown <shivamrajput81202@gmail.com>
2025-02-24 23:31:02 +0530

    [Shivam] add:Assignment Questions on Annotations

---
+++ Assignment23 Annotation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    public static void Main(){
+        // Test1.Print();
+        // Test2.Print();
+        // Test3.Print();
+        // Test4.Print();
+        Test5.Print();
+        Console.ReadKey();
+        Test6.Print();
+        Console.ReadKey();
+        Test7.Print();
+        Console.ReadKey();
+        Test8.Print();
+        Console.ReadKey();
+        Test9.Print();
+        Console.ReadKey();
+        Test10.Print();
+        Console.ReadKey();
+        Test11.Print();
+        Console.ReadKey();
+        Test12.Print();
+        Console.ReadKey();
+    }
+}
